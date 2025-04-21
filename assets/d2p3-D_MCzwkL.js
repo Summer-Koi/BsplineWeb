1a2c47f08d718b69302fc9db6d43f2e7fd2680b4
Summer-Koi <Summer-Koi@users.noreply.github.com>
2025-04-21 10:09:00 +0000

    deploy: 92bf368effa910ea0239b588c0142330c8679fd2

---
+++ assets/d2p3-D_MCzwkL.js
@@ -0,0 +1,1 @@
+const e=2,o=[0,0,0,1,1,1],t=[{x:161.5,y:438.203125},{x:201.5,y:254.203125},{x:443.5,y:366.203125}],n={degree:2,knots:o,controlPoints:t};export{t as controlPoints,n as default,e as degree,o as knots};
